c48d8eee310aa229ee21f9bec672c2353c4e0d5d
Izzat <izzyzizou@ymail.com>
2016-05-14 18:55:12 -0400

    init commit

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+//import java.util.*;
+
+public class hello{
+    //private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        studentData student1 = new studentData("John", "Doe", 89, 78, 95, 63, 94);
+        studentData student2 = new studentData("Izzat", "Abdu", 92, 82, 90, 70, 87, 99);
+
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
